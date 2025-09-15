52aba2ff656e44d3e3a72ef6667fd1e12b2621b4
Iftikher Lutfur <iftikherlutfur@gmail.com>
2025-09-15 16:02:33 +0600

    Fix and error

--- app/Components/Slide1.jsx
+++ app/Components/Slide1.jsx
@@ -54,7 +54,7 @@ const Slide1 = () => {
             alt="Slide 3"
             width={960}
             height={540}
-            className="w-full h-[590px] object-cover"
+            className="w-full h-auto object-cover"
           />
         </SwiperSlide>
         <SwiperSlide>
